dfe8ace24702c510f9ff0c57327650b4be480a9a
Fabricio Valadares <fabricio.valadares@gmail.com>
2021-02-08 22:14:15 -0300

    Refactoring classes

--- app/src/main/java/dev/estudos/calculadoraimc/MainActivity.kt
+++ app/src/main/java/dev/estudos/calculadoraimc/MainActivity.kt
@@ -8,7 +8,7 @@ import dev.estudos.calculadoraimc.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
