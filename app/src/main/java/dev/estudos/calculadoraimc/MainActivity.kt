33746ff0cfc5113993f7f5ca3a64b2608dab0fde
Fabricio Valadares <fabricio.valadares@gmail.com>
2021-02-09 22:56:49 -0300

    Refactoring classes

--- app/src/main/java/dev/estudos/calculadoraimc/MainActivity.kt
+++ app/src/main/java/dev/estudos/calculadoraimc/MainActivity.kt
@@ -19,7 +19,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-
         val view = binding.root
         setContentView(view)
 
@@ -28,42 +27,51 @@ class MainActivity : AppCompatActivity() {
 
     private fun initComponents() {
         title = "BMI Calculator"
+
         binding.btActivityMainCalc.setOnClickListener {
-            when {
-                binding.etActivityMainWeight.text.isBlank() -> {
-                    Toast.makeText(
-                        this,
-                        "Please, inform the weight.",
-                        Toast.LENGTH_SHORT
-                    ).show()
-                    binding.etActivityMainWeight.requestFocus()
-                }
-
-                binding.etActivityMainHeight.text.isBlank() -> {
-                    Toast.makeText(
-                        this,
-                        "Please, inform the Height.",
-                        Toast.LENGTH_SHORT
-                    ).show()
-                    binding.etActivityMainHeight.requestFocus()
-                }
-                else -> {
-                    val weight = binding.etActivityMainWeight.text.toString().toBigDecimal()
-                    val height = binding.etActivityMainHeight.text.toString().toBigDecimal()
-                    val result = BMI().calc(weight, height)
-                    Log.d("MainActivity", result.toString())
-                    val intent = Intent(this, ResultActivity::class.java).apply {
-                        putExtra(TAG, result.toString())
-                    }
-                    startActivity(intent)
-
-                }
-            }
+            actionOfBtActivityMainCalc()
         }
 
         binding.btActivityMainClear.setOnClickListener {
-            binding.etActivityMainHeight.text.clear()
-            binding.etActivityMainWeight.text.clear()
+            actionOfBtActivityMainClear()
+        }
+    }
+
+    private fun actionOfBtActivityMainClear() {
+        binding.etActivityMainHeight.text.clear()
+        binding.etActivityMainWeight.text.clear()
+    }
+
+    private fun actionOfBtActivityMainCalc() {
+        when {
+            binding.etActivityMainWeight.text.isBlank() -> {
+                Toast.makeText(
+                    this,
+                    "Please, inform the weight.",
+                    Toast.LENGTH_SHORT
+                ).show()
+                binding.etActivityMainWeight.requestFocus()
+            }
+
+            binding.etActivityMainHeight.text.isBlank() -> {
+                Toast.makeText(
+                    this,
+                    "Please, inform the Height.",
+                    Toast.LENGTH_SHORT
+                ).show()
+                binding.etActivityMainHeight.requestFocus()
+            }
+            else -> {
+                val weight = binding.etActivityMainWeight.text.toString().toBigDecimal()
+                val height = binding.etActivityMainHeight.text.toString().toBigDecimal()
+                val result = BMI().calc(weight, height)
+                Log.d("MainActivity", result.toString())
+                val intent = Intent(this, ResultActivity::class.java).apply {
+                    putExtra(TAG, result.toString())
+                }
+                startActivity(intent)
+
+            }
         }
     }
 }
